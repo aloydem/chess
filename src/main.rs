c12d4c4e251b5acaaf06e93d927f256318b32b07
aloydem <aloysiusdemiurgos@gmail.com>
2024-08-01 14:09:13 -0500

    [feat]: Add the definition of chess piece

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-mod chess {}
+mod chess {
+    enum Color {
+        Black,
+        White,
+    }
+
+    enum Rank {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King,
+    }
+
+    struct Piece {
+        color: Color,
+        rank: Rank,
+    }
+}
 
 fn main() {
     println!("Hello, world!");
